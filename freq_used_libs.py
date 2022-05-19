77b80959bb75623f0c5a20298f00cc510636b273
Pradeep Singh <viveksingh12126@gmail.com>
2022-05-19 10:28:53 +0530

    Create freq_used_libs.py

---
+++ freq_used_libs.py
@@ -0,0 +1,30 @@
+# Baic Libraries
+import numpy as np
+import pandas as pd
+
+# Visualisation
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+# Missing
+import missingno as msno
+
+# Scientific Libraries
+from scipy import stats
+from scipy.stats import norm
+from scipy.stats import kurtosis, skew
+
+# ML Modeling
+import xgboost as xgb
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.feature_selection import chi2
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_selection import SelectKBest
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.ensemble import GradientBoostingRegressor
